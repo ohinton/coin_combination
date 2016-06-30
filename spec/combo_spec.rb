20ed9e74c5d89bb621bd0cc20fb3922fbcbc46c7
Olivia Hinton <hinton.olivia@gmail.com>
2016-06-30 13:13:12 -0700

    fix integration spec

--- spec/combo_spec.rb
+++ spec/combo_spec.rb
@@ -1,17 +1,17 @@
 require('rspec')
 require('combo')
 
-describe('Fixnum#combo') do
+describe('String#combo') do
   it("returns the amount of quarters from any given number") do
-    expect((25).combo).to eq("1 quarters")
+    expect(('25').combo).to eq("1 quarters")
   end
   it("returns the amount of dimes from any given number") do
-    expect((10).combo).to eq("1 dimes")
+    expect(('10').combo).to eq("1 dimes")
   end
   it("returns the amount of nickels from any given number") do
-    expect((5).combo).to eq("1 nickels")
+    expect(('5').combo).to eq("1 nickels")
   end
   it("returns the amount of pennies from any given number") do
-    expect((1).combo).to eq("1 pennies")
+    expect(('1').combo).to eq("1 pennies")
   end
 end
